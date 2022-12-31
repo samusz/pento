4144e0f36c5b3283a5e3888e92127959b5191d7f
Sacha Muszlak <sacha@muszlak.com>
2023-01-01 00:32:57 +0100

    Add seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+alias Pento.Catalog
+
+products = [
+  %{
+    name: "Chess",
+    description: "The classic strategy game",
+    sku: 5_678_910,
+    unit_price: 10.00
+  },
+  %{
+    name: "Tic-Tac-Toe",
+    description: "The game of Xs and Os",
+    sku: 11_121_314,
+    unit_price: 3.00
+  }
+]
+
+Enum.each(products, fn products -> Catalog.create_product(product)) end
